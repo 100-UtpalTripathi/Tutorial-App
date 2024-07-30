6e2134215f0dd256020ed9bb70833e94b2239b2d
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-07-30 12:54:11 +0530

    Frontend Project and folder structure setup using react!

---
+++ frontend/src/components/user/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RecommendedCourses from '../RecommendedCourses/RecommendedCourses';
+
+const Home = () => {
+  return (
+    <div className="container mt-4">
+      <h1>Recommended Courses</h1>
+      <RecommendedCourses />
+    </div>
+  );
+};
+
+export default Home;
